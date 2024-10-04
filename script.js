0cbd8f8a1bee9ab728bb62cf1664a52ae69fac9d
Charlles69 <Charlles.ribeiro@escola.pr.gov.br>
2024-10-04 00:52:30 -0300

    arrumei tudinho

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.querySelector('#contact form').addEventListener('submit', function(even
     alert('Sua mensagem foi enviada com sucesso!');
 });
 
-document.querySelector('.login-container form').addEventListener('submit', function(event) {
+document.querySelector('#register form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('seu registro foi efetuado');
 });
 
 document.querySelector('#login form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Seu login foi efetivado com Sucesso');
+    alert('Seu login foi efetivado');
 });
